f5b7d0135efdd176d708b1b18dfe39cda24be88c
Ruan Xavier <ruandsxavier@gmail.com>
2021-05-11 08:36:46 -0300

    adding createCategory usecase

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,13 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/usecases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategory = new CreateCategoryService(categoriesRepository);
-
-  createCategory.execute({ name, description });
-
-  return response.status(201).send();
+  return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
