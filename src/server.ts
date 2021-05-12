5b4aacec1bd2ebc338bc288c8cd9e0e00764aa13
Ruan Xavier <ruandsxavier@gmail.com>
2021-05-12 09:11:46 -0300

    adding swagger documentation (not so good)

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { serve, setup } from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerOptions from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+app.use("/docs", serve, setup(swaggerOptions));
 app.use(router);
 
 app.listen(3333, () => console.log(`Server started at port ${3333}`));
