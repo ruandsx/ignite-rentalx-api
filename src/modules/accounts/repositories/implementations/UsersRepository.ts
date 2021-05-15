af1d3f316129fac29377ec50afc375a025485d51
Ruan Xavier <ruandsxavier@gmail.com>
2021-05-15 13:06:46 -0300

    verifying edge cases at user creation

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -28,6 +28,21 @@ class UsersRepository implements IUsersRepository {
 
     await this.repository.save(user);
   }
+
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.repository.findOne({ email });
+    return user;
+  }
+
+  async findByUsername(username: string): Promise<User> {
+    const user = await this.repository.findOne({ username });
+    return user;
+  }
+
+  async findByDriverLicense(driver_license: string): Promise<User> {
+    const user = await this.repository.findOne({ driver_license });
+    return user;
+  }
 }
 
 export { UsersRepository };
--- src/modules/accounts/usecases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/usecases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -17,11 +18,29 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userWithSameEmail = await this.usersRepository.findByEmail(email);
+    if (userWithSameEmail) {
+      throw new Error("Email already in use");
+    }
+    const userWithSameUsername = await this.usersRepository.findByUsername(
+      username
+    );
+    if (userWithSameUsername) {
+      throw new Error("Username already in use");
+    }
+    const userWithSameDriverLicense = await this.usersRepository.findByDriverLicense(
+      driver_license
+    );
+    if (userWithSameDriverLicense) {
+      throw new Error("Driver license already in use");
+    }
+
+    const passwordHash = await hash(password, 12);
     await this.usersRepository.create({
       name,
       email,
       username,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
